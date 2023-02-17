44eefdfe68e7461dd0de5aeafcfef77fa59a8115
Mouse Wanderer <108565870+mousewanderer@users.noreply.github.com>
2023-02-17 09:55:44 +0800

    Adding a download function
    download function to the script so that they can download the modules required for them to run if they want to use it

---
+++ dowload_script.py
@@ -0,0 +1,13 @@
+import os, time
+
+
+# Use pip to download and install modules
+
+list_of_modules =["urllib","numpy","discord.py",
+                  "beautifulsoup4","request","json",
+                  "difflib"]
+
+
+for downloads in list_of_modules:
+    time.sleep(1)
+    os.system(f'pip install {downloads}')
